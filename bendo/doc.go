2c251a5c9a0269d27e77d048da1d3401ee01153d
Don Brower <dbrower@nd.edu>
2015-07-17 16:29:00 -0400

    Revise docs

--- bendo/doc.go
+++ bendo/doc.go
@@ -1,14 +1,30 @@
 /*
 
-Each item consists of a list of blobs and a list of versions. Each blob contains
-binary content. Each version maps symbolic names to specific blobs. Items do not
-share blobs or versions with other items.
+Bendo provides routines to manulipate and serialize items.
+An item is defined as a collection of blobs and versions.
+Blobs are immutable binary blocks of data. Blobs may be added to an item over
+time, and may be deleted, but they cannot be otherwise altered once added.
+Versions provide a way to associate labels to blobs, and may be added to an
+item over time, but cannot be deleted.
 
-Blobs are numbered from 1. The blob with id 0 has the special meaning of the
-empty length stream.
+Items are serialized into a sequence of bundles. Each bundle file is immutable.
+However, like blobs, bundle files may be deleted.
 
+Items do not share blobs between them. Bundle files do not contain information
+from more than one item.
 
-An item's metadata and blobs are grouped into "bundles", which are zip files.
+Two levels of interfaces are provided. An ItemStore provides the logic
+to do the serialization and deserialization of items to bundles. It wraps
+a BundleStore interface. It will block, and is intended to be used inside a
+single goroutine.
+
+The other level of interface is the Server, which provides caching, and
+grouping of requests so that many requests for an item from different gorutines
+will only spawn a single request to the underlying BundleStore.
+
+Both blobs and versions are numbered sequentually starting from 1.
+
+An item's metadata and blobs are grouped into bundles, which are zip files.
 Each bundle contains the complete up-to-date metadata information on an item,
 as well as zero or more blobs. Bundles are numbered, but they should not be
 assumed to be numbered sequentially since deletions may remove some bundles.
