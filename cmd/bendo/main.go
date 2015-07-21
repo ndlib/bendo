1ef9f8eb7dd7b65565b699181529cfa34544d7fc
Don Brower <dbrower@nd.edu>
2015-07-21 16:47:14 -0400

    More server. Support item.store

--- cmd/bendo/main.go
+++ cmd/bendo/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"net/http"
 
+	"github.com/ndlib/bendo/bendo"
 	"github.com/ndlib/bendo/server"
 )
 
 func main() {
+	server.Items = bendo.New(bendo.NewFileStore("."))
 	http.ListenAndServe(":14000", server.AddRoutes())
 }
--- server/routes.go
+++ server/routes.go
@@ -1,19 +1,50 @@
-package http
+package server
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"strconv"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
+
+	"github.com/ndlib/bendo/bendo"
+)
+
+var (
+	Items *bendo.Store
 )
 
 func AddRoutes() http.Handler {
-	r := mux.NewRouter()
-	r.HandleFunc("/item/{id}/blob/{bid}", BlobHandler)
+	r := httprouter.New()
+	r.Handle("GET", "/blob/:id/:bid", BlobHandler)
+	r.Handle("GET", "/item/:id", ItemHandler)
+	r.Handle("GET", "/item/:id/:version/:slot", SlotHandler)
 	return r
 }
 
-func BlobHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	fmt.Fprintf(w, "Hello, item %s with blob %s", vars["id"], vars["bid"])
+func BlobHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "Blob %s/%s\n", ps.ByName("id"), ps.ByName("bid"))
+	n, err := strconv.ParseInt(ps.ByName("bid"), 10, 0)
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+	src, err := Items.Blob(ps.ByName("id"), bendo.BlobID(n))
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+	io.Copy(w, src)
+}
+
+func ItemHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "Hello, item %s", ps.ByName("id"))
+}
+
+func SlotHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "Hello, item %s with version %s slot %s",
+		ps.ByName("id"),
+		ps.ByName("version"),
+		ps.ByName("slot"))
 }
