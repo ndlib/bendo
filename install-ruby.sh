98e3f4815ef0596379a4f2363a488629e53833f0
Sean Summers <ssummer3@nd.edu>
2017-11-01 11:01:48 -0400

    curl fixes

--- install-ruby.sh
+++ install-ruby.sh
@@ -1,9 +1,9 @@
-#! /bin/bash
+#! /bin/bash -xe
 
 # ruby 2.4.1
 version=2.4.1
 cd /usr/local/src
-curl -O https://cache.ruby-lang.org/pub/ruby/2.4/ruby-$version.tar.gz
+curl -sO https://cache.ruby-lang.org/pub/ruby/2.4/ruby-$version.tar.gz
 tar zxvf ruby-$version.tar.gz
 cd ruby-$version
 ./configure
@@ -13,7 +13,7 @@ make install
 # ruby-gems
 version=2.6.12
 cd ..
-curl -O https://rubygems.org/rubygems/rubygems-$version.tgz
+curl -sO https://rubygems.org/rubygems/rubygems-$version.tgz
 tar zxvf rubygems-$version.tgz
 cd rubygems-$version
 /usr/local/bin/ruby setup.rb
