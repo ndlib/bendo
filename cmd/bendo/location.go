e054332a27a4aa00010addd5cee352e0d49886cf
Don Brower <dbrower@nd.edu>
2020-12-03 16:54:51 -0500

    Add ability to configure BlackPearl endpoint
    This adds a "blackpearl://" and "blackpearls://" schemes to the path
    used to give the `StoreDir`. This lets a URL be given, and when that
    scheme is given a BlackPearl driver is created.
    
    This moves a lot of the server configuration from the server code into
    cmd/bendo where it makes more sense. There were lots of fields passed
    into the server that were only used at startup. Now the initial
    executiable is responsible for reading the config file and bootstrapping
    the server pieces.

---
+++ cmd/bendo/location.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"log"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/SpectraLogic/ds3_go_sdk/ds3"
+	"github.com/SpectraLogic/ds3_go_sdk/ds3/networking"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+
+	"github.com/ndlib/bendo/store"
+)
+
+// splitBucketPrefix will take a path and separate the bucket name from a prefix, if any.
+// It will also append "addtion" to the prefix, and make sure the prefix returned is
+// either empty or ends with a slash "/".
+//
+// examples:
+// 		"" -> ("", "")
+//		"bucket" -> ("bucket", "")
+//		"bucket/and/a/prefix" -> ("bucket", "and/a/prefix/")
+func splitBucketPrefix(location string, addition string) (bucket, prefix string) {
+	if location == "" {
+		return
+	}
+	location = strings.TrimPrefix(location, "/")
+	v := strings.SplitN(location, "/", 2)
+	bucket = v[0]
+	if len(v) > 1 {
+		prefix = v[1]
+	}
+	if addition != "" {
+		prefix = path.Join(prefix, addition)
+	}
+	if prefix != "" && !strings.HasSuffix(prefix, "/") {
+		prefix = prefix + "/"
+	}
+	return
+}
+
+// parselocation will create an approprate store based on "location".
+// In case of an error, nil is returned.
+// If location is empty, a memory store is returned.
+// It understands special schemes "s3:" and "blackpearl:".
+// Use "blackpearls:" for a https connection to a BlackPearl device.
+func parselocation(location string, addition string) store.Store {
+	if location == "" {
+		return store.NewMemory()
+	}
+	u, _ := url.Parse(location)
+	switch u.Scheme {
+	case "", "file":
+		path := filepath.Join(u.Path, addition)
+		os.MkdirAll(path, 0755)
+		return store.NewFileSystem(path)
+	case "s3":
+		conf := &aws.Config{}
+		if u.Host != "" {
+			conf.Endpoint = aws.String(u.Host)
+			conf.Region = aws.String("us-east-1")
+			// disable SSL for local development
+			if strings.Contains(u.Host, "localhost") {
+				conf.DisableSSL = aws.Bool(true)
+				conf.S3ForcePathStyle = aws.Bool(true)
+			}
+		}
+		bucket, prefix := splitBucketPrefix(u.Path, addition)
+		if bucket == "" {
+			log.Println("Error parsing location, no bucket name", location)
+			return nil
+		}
+		return store.NewS3(bucket, prefix, session.New(conf))
+	case "blackpearl", "blackpearls":
+		accessKey := os.Getenv("DS3_ACCESS_KEY")
+		secretKey := os.Getenv("DS3_SECRET_KEY")
+		switch {
+		case accessKey == "":
+			panic("DS3_ACCESS_KEY missing")
+		case secretKey == "":
+			panic("DS3_SECRET_KEY missing")
+		}
+		// build the URL for the blackpearl
+		endpoint := &url.URL{
+			Scheme: "http",
+			Host:   u.Host,
+		}
+		if u.Host == "blackpearls" {
+			endpoint.Scheme = "https"
+		}
+		bucket, prefix := splitBucketPrefix(u.Path, addition)
+		if bucket == "" {
+			log.Println("Error parsing location, no bucket name", location)
+			return nil
+		}
+		bp := ds3.NewClientBuilder(
+			endpoint,
+			&networking.Credentials{AccessId: accessKey, Key: secretKey},
+		).BuildClient()
+		return store.NewBlackPearl(bucket, prefix, bp)
+
+	}
+	// there was some kind of error. Return a Memory store? or fail?
+	log.Println("Problem parsing location", location)
+	return nil
+}
--- cmd/bendo/main.go
+++ cmd/bendo/main.go
@@ -5,13 +5,18 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 	"time"
 
 	"github.com/BurntSushi/toml"
+
+	"github.com/ndlib/bendo/blobcache"
+	"github.com/ndlib/bendo/fragment"
 	"github.com/ndlib/bendo/items"
 	"github.com/ndlib/bendo/server"
 	"github.com/ndlib/bendo/store"
+	"github.com/ndlib/bendo/transaction"
 	// logs all http requests. useful for debugging S3
 	//	_ "github.com/motemen/go-loghttp/global"
 )
@@ -32,9 +37,10 @@ type bendoConfig struct {
 }
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 
 	// Start with the Default values
-	config := bendoConfig{
+	config := &bendoConfig{
 		StoreDir:     ".",
 		Tokenfile:    "",
 		CacheDir:     "",
@@ -48,65 +54,50 @@ func main() {
 	}
 
 	var configFile = flag.String("config-file", "", "Configuration File")
-
 	flag.Parse()
-
-	// If config file arg provided, try to open & decode it
+	// If config file name is provided, try to open & decode it
 	if *configFile != "" {
 		log.Printf("Using config file %s\n", *configFile)
-		if _, err := toml.DecodeFile(*configFile, &config); err != nil {
+		if _, err := toml.DecodeFile(*configFile, config); err != nil {
 			log.Println(err)
 			return
 		}
 	}
 
-	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	log.Println("==========")
+	log.Println("Starting Bendo Server version", server.Version)
+	log.Println("StoreDir =", config.StoreDir)
+	log.Println("CacheDir =", config.CacheDir)
+	log.Println("CacheSize =", config.CacheSize)
+	log.Println("CacheTimeout =", config.CacheTimeout)
 
-	log.Printf("Using storage dir %s\n", config.StoreDir)
-	log.Printf("Using cache dir %s\n", config.CacheDir)
-	log.Printf("Using cache timeout %s\n", config.CacheTimeout)
-	var validator server.TokenValidator
-	if config.Tokenfile != "" {
-		var err error
-		log.Printf("Using user token file %s\n", config.Tokenfile)
-		validator, err = server.NewListValidatorFile(config.Tokenfile)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-	} else {
-		log.Printf("No user token file specified")
-		validator = server.NobodyValidator{}
-	}
-	var itemstore store.Store = store.NewFileSystem(config.StoreDir)
-	if config.CowHost != "" {
-		log.Printf("Using COW with target %s", config.CowHost)
-		itemstore = store.NewCOW(itemstore, config.CowHost, config.CowToken)
-	}
-	timeout, _ := time.ParseDuration(config.CacheTimeout)
-	var s = server.RESTServer{
-		Items:        items.New(itemstore),
-		Validator:    validator,
-		MySQL:        config.Mysql,
-		CacheDir:     config.CacheDir,
-		CacheSize:    config.CacheSize * 1000000,
-		CacheTimeout: timeout,
-		PortNumber:   config.PortNumber,
-		PProfPort:    config.PProfPort,
-	}
-	if config.CowHost != "" {
-		// don't run fixity if we are using a copy-on-write.
-		// (doing so will cause us to download ALL the data from
-		// the target bendo over time)
-		s.DisableFixity = true
+	// use the config values to set up the server
+	var s = &server.RESTServer{
+		Items:      nil,
+		Validator:  nil,
+		PortNumber: config.PortNumber,
+		PProfPort:  config.PProfPort,
 	}
 
-	// set up signal handlers
+	// Use the config settings to update s.
+	// All the setup* functions panic on error.
+	setupTokens(config, s)
+	// set up preservation store. Do this before setting up the database.
+	setupItemStore(config, s)
+	setupCache(config, s)
+	setupTransactionStore(config, s)
+	setupUploadStore(config, s)
+	setupDatabase(config, s)
+
+	// install signal handlers
 	sig := make(chan os.Signal, 5)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-	go signalHandler(sig, &s)
+	go signalHandler(sig, s)
 
-	s.Run()
+	err := s.Run()
+	if err != nil {
+		log.Println(err)
+	}
 	log.Println("Exiting")
 }
 
@@ -118,4 +109,98 @@ func signalHandler(sig <-chan os.Signal, svr *server.RESTServer) {
 			svr.Stop() // this will cause Run to exit
 		}
 	}
+}
+
+// setupItemStore uses config to mutate s to add the item store.
+// It will panic on error.
+func setupItemStore(config *bendoConfig, s *server.RESTServer) {
+	itemstore := parselocation(config.StoreDir, "")
+	if itemstore == nil {
+		panic("no storage location")
+	}
+	if config.CowHost != "" {
+		log.Printf("Using COW with target %s", config.CowHost)
+		itemstore = store.NewCOW(itemstore, config.CowHost, config.CowToken)
+
+		// don't run fixity if we are using a copy-on-write.
+		// (doing so will cause us to download ALL the data from
+		// the target bendo over time)
+		s.DisableFixity = true
+	}
+	s.Items = items.New(itemstore)
+}
+
+// setupTokens configures the token verification. It will panic on error.
+func setupTokens(config *bendoConfig, s *server.RESTServer) {
+	if config.Tokenfile != "" {
+		var err error
+		log.Printf("Using user token file %s\n", config.Tokenfile)
+		s.Validator, err = server.NewListValidatorFile(config.Tokenfile)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		log.Printf("No user token file specified")
+		s.Validator = server.NobodyValidator{}
+	}
+}
+
+func setupCache(config *bendoConfig, s *server.RESTServer) {
+	timeout, _ := time.ParseDuration(config.CacheTimeout)
+	size := config.CacheSize * 1000000 // config is in MB
+	if config.CacheDir == "" || size == 0 {
+		log.Println("Not using blob cache")
+		s.Cache = blobcache.EmptyCache{}
+	} else {
+		v := parselocation(config.CacheDir, "blobcache")
+		if v == nil {
+			panic("no location for cache")
+		}
+		if timeout != 0 {
+			log.Println("Using time-based cache strategy")
+			s.Cache = blobcache.NewTime(v, timeout)
+		} else {
+			log.Println("Using size-based cache strategy")
+			s.Cache = blobcache.NewLRU(v, size)
+		}
+	}
+}
+
+func setupTransactionStore(config *bendoConfig, s *server.RESTServer) {
+	v := parselocation(config.CacheDir, "transaction")
+	s.TxStore = transaction.New(v)
+}
+
+func setupUploadStore(config *bendoConfig, s *server.RESTServer) {
+	v := parselocation(config.CacheDir, "upload")
+	s.FileStore = fragment.New(v)
+}
+
+func setupDatabase(config *bendoConfig, s *server.RESTServer) {
+	var db interface {
+		server.FixityDB
+		items.ItemCache
+	}
+	var err error
+	if config.Mysql != "" {
+		log.Printf("Using MySQL")
+		db, err = server.NewMysqlCache(config.Mysql)
+	} else {
+		var path string
+		// this gets wonky if the cacheDir is an s3: path. but it still works!
+		// (it makes a file system directory named "s3:")
+		if config.CacheDir != "" {
+			os.MkdirAll(config.CacheDir, 0755)
+			path = filepath.Join(config.CacheDir, "bendo.ql")
+		} else {
+			path = "memory"
+		}
+		log.Println("Using internal database at", path)
+		db, err = server.NewQlCache(path)
+	}
+	if db == nil || err != nil {
+		panic("problem setting up database")
+	}
+	s.FixityDatabase = db
+	s.Items.SetCache(db)
 }
